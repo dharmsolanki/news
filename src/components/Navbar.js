fb36995bd971f25f8c7648fc28d2896986e4a519
dharmsolanki <dharmsolanki1010@gmail.com>
2023-08-21 12:40:58 +0530

    add categories in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,17 @@ export class Navbar extends Component {
             <a className="navbar-brand" href="/">
               News-App
             </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
             <div
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
@@ -23,6 +34,41 @@ export class Navbar extends Component {
                 <li className="nav-item">
                   <a className="nav-link" href="/about">
                     About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    business
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    entertainment
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    general
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    health
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    science
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    sports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
+                    technology
                   </a>
                 </li>
               </ul>
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
   constructor() {
     super();
     this.state = {
@@ -14,7 +28,7 @@ export class News extends Component {
 
   async componentDidMount() {
     let url =
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=54185d6ce85b416eb42e20361b29436e&pageSize=${this.props.pageSize}`;
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=54185d6ce85b416eb42e20361b29436e&pageSize=${this.props.pageSize}`;
     this.setState({loading:true})
     let data = await fetch(url);
     let parseData = await data.json();
@@ -26,7 +40,7 @@ export class News extends Component {
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=54185d6ce85b416eb42e20361b29436e&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=54185d6ce85b416eb42e20361b29436e&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
@@ -43,7 +57,7 @@ export class News extends Component {
   handleNextClick = async () => {
     if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
 
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=54185d6ce85b416eb42e20361b29436e&page=${
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=54185d6ce85b416eb42e20361b29436e&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});
@@ -60,7 +74,7 @@ export class News extends Component {
 
   render() {
     return (
-      <div className="container my-3">
+      <div className="container my-4">
         <h2 className="text-center">Top Headlines</h2>
         {this.state.loading && <Spinner/>}
         <div className="row">
