d3fe0d9766b44b3a9ec1b689e0ea248a238dc632
dharmsolanki <dharmsolanki1010@gmail.com>
2023-08-31 16:12:33 +0530

    add infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -16,17 +17,25 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
+      totalResults: 0,
     };
+
+    document.title = `${this.capitalizeFirstLetter(
+      this.props.category
+    )} - News`;
   }
 
   async updateNews() {
-
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=54185d6ce85b416eb42e20361b29436e&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -36,7 +45,6 @@ export class News extends Component {
       totalResults: parseData.totalResults,
       loading: false,
     });
-
   }
 
   async componentDidMount() {
@@ -71,7 +79,7 @@ export class News extends Component {
     //   loading: false,
     // });
 
-    this.setState({page:this.state.page - 1})
+    this.setState({ page: this.state.page - 1 });
     this.updateNews();
   };
 
@@ -102,67 +110,68 @@ export class News extends Component {
     //   });
     // }
 
-    this.setState({page:this.state.page + 1})
+    this.setState({ page: this.state.page + 1 });
     this.updateNews();
   };
 
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=54185d6ce85b416eb42e20361b29436e&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+
+    this.setState({
+      articles: this.state.articles.concat(parseData.articles),
+      totalResults: parseData.totalResults,
+    });
+  };
+
   render() {
+
+    const isLoading = this.state.loading || this.state.articles.length === 0;
+
+     // Calculate hasMoreArticles based on the current page and totalResults
+     const hasMoreArticles = this.state.page * this.props.pageSize < this.state.totalResults;
+
     return (
-      <div className="container my-4">
+      <>
         <h2 className="text-center">Top Headlines</h2>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this?.state?.articles?.map((element) => {
-              return (
-                <div className="col-md-3 my-2" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title.slice(0, 45) : ""}
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-                    imageUrl={
-                      !element.urlToImage
-                        ? "https://i.ytimg.com/vi/o7u-OAVf_Eg/maxresdefault.jpg"
-                        : element.urlToImage
-                    }
-                    newsUrl={element.url}
-
-                    author={element.author}
-
-                    date={element.publishedAt}
-
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-
-        <div className="container d-flex justify-content-between">
-          <button
-            type="button"
-            disabled={this.state.page <= 1}
-            className="btn btn-dark"
-            onClick={this.handlePrevClick}
-          >
-            &laquo; Previous
-          </button>
-          <button
-            type="button"
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
-            Next &raquo;
-          </button>
-        </div>
-      </div>
+         {isLoading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={hasMoreArticles} // Use the calculated hasMoreArticles value
+          loader={isLoading ? null : <Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this?.state?.articles?.map((element) => {
+                return (
+                  <div className="col-md-3 my-2" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title.slice(0, 45) : ""}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 88)
+                          : ""
+                      }
+                      imageUrl={
+                        !element.urlToImage
+                          ? "https://i.ytimg.com/vi/o7u-OAVf_Eg/maxresdefault.jpg"
+                          : element.urlToImage
+                      }
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
     );
   }
 }
